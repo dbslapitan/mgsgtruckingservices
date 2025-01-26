8011d965f583cd8b2556ae2410a4a805e8f503d0
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-01-26 06:36:47 -0500

    change a tags to link in contact us

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,5 +1,6 @@
 import Map from "@/ui/map/map";
 import styles from "./page.module.scss";
+import Link from "next/link";
 
 
 export default function Page(){
@@ -11,9 +12,9 @@ export default function Page(){
                     <br />
                     Cebu, Philippines
             </address>
-            <a href="tel:+639628028946" className={`${styles["contacts__detail"]}`}><strong>SMART:</strong> (+63) 962 802 8946</a>
-            <a href="tel:+639674534460" className={`${styles["contacts__detail"]}`}><strong>GLOBE:</strong> (+63) 967 453 4460</a>
-            <a href="mailto:lapitan@mgsgtruckingservices.com?subject=Business Inquiry" className={`${styles["contacts__detail"]}`}>lapitan@mgsgtruckingservices.com</a>
+            <Link href="tel:+639628028946" className={`${styles["contacts__detail"]}`}><strong>SMART:</strong> (+63) 962 802 8946</Link>
+            <Link href="tel:+639674534460" className={`${styles["contacts__detail"]}`}><strong>GLOBE:</strong> (+63) 967 453 4460</Link>
+            <Link href="mailto:lapitan@mgsgtruckingservices.com?subject=Business Inquiry" className={`${styles["contacts__detail"]}`}>lapitan@mgsgtruckingservices.com</Link>
             <Map/>
         </main>
     );
