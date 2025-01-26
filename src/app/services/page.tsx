d3f32ba98dfd8469192c61fd64091566222b6d13
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-01-26 06:04:34 -0500

    finish about us on all media

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,8 +1,28 @@
+import Image from "next/image";
 import styles from "./page.module.scss";
+import trucking from "../../../public/assets/transport.png";
+import hauling from "../../../public/assets/dump.png";
+import aggregates from "../../../public/assets/gravel.png";
 
 
 export default function Page(){
     return(
-        <h1>Services</h1>
+        <main className={`${styles["services"]}`}>
+            <h1 className={`${styles["services__title"]}`}>Our Services</h1>
+            <ul className={`${styles["services__list"]}`}>
+                <li className={`${styles["services__item"]}`}>
+                    <Image className={`${styles["services__image"]}`} src={trucking} width={512} height={512} alt=""/>
+                    <h2 className={`${styles["services__sub"]}`}>Trucking</h2>
+                </li>
+                <li className={`${styles["services__item"]}`}>
+                    <Image className={`${styles["services__image"]} ${styles["services__image--hauling"]}`} src={hauling} width={512} height={512} alt=""/>
+                    <h2 className={`${styles["services__sub"]}`}>Hauling</h2>
+                </li>
+                <li className={`${styles["services__item"]}`}>
+                    <Image className={`${styles["services__image"]} ${styles["services__image--hauling"]}`} src={aggregates} width={512} height={512} alt=""/>
+                    <h2 className={`${styles["services__sub"]}`}>Sand & Gravel</h2>
+                </li>
+            </ul>
+        </main>
     );
 }
