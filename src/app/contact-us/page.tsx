afb91223804c3a53bad16c150e44bd573e064302
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2025-03-18 10:30:48 -0400

    uncomment smart

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -12,7 +12,7 @@ export default function Page(){
                     <br />
                     Cebu, Philippines
             </address>
-            {/* <Link href="tel:+639628028946" className={`${styles["contacts__detail"]}`}><strong>SMART:</strong> (+63) 962 802 8946</Link> */}
+            <Link href="tel:+639628028946" className={`${styles["contacts__detail"]}`}><strong>SMART:</strong> (+63) 962 802 8946</Link>
             <Link href="tel:+639674534460" className={`${styles["contacts__detail"]}`}><strong>GLOBE:</strong> (+63) 917 109 6379</Link>
             <Link href="mailto:lapitan@mgsgtruckingservices.com?subject=Business Inquiry" className={`${styles["contacts__detail"]}`}>lapitan@mgsgtruckingservices.com</Link>
             <Map/>
